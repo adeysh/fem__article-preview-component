e51020e61f04803c8d158d04003aa1a159a56e9c
Adesh Katiya <adeshkatiya27@gmail.com>
2025-03-21 20:18:08 +0530

    feat: add keyboard escape functionality to close social sharing

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,9 @@ shareBtn.addEventListener("click", () => {
     shareBtn.classList.toggle("product-card__share-button--active");
     socialContainer.classList.toggle("product-card__social--active");
 });
+
+document.addEventListener("keydown", (event) => {
+    if (socialContainer.classList.contains("product-card__social--active") && event.code === "Escape") {
+        socialContainer.classList.remove("product-card__social--active");
+    }
+})
