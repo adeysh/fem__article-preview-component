60bcc12dc652a5c50d054dbb2242cd3de8299d35
Adesh Katiya <adeshkatiya27@gmail.com>
2025-04-07 17:21:57 +0530

    refactor(script): extract toggle functionality for share button

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,13 @@
 const shareBtn = document.getElementById("shareBtn");
 const socialContainer = document.getElementById("socialContainer");
 
-shareBtn.addEventListener("click", () => {
+const toggleClass = () => {
     shareBtn.classList.toggle("product-card__share-button--active");
     socialContainer.classList.toggle("product-card__social--active");
+}
+
+shareBtn.addEventListener("click", () => {
+    toggleClass();
 });
 
 document.addEventListener("keydown", (event) => {
